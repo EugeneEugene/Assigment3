e42429cbf438aebab526e0b3f4d74be83ef136cd
Uknown_player <evgeniiledin@gmail.com>
2018-05-03 18:52:25 +0200

    add functionality to "New Game Button"

--- Assigment3/ViewController.swift
+++ Assigment3/ViewController.swift
@@ -2,10 +2,16 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var playingTableOutlet: PlayingTableView!
+    var deck = DeckOfCards().deck
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var deck = DeckOfCards().deck
+        deck = DeckOfCards().deck
+        deck.shuffle()
+        playingTableOutlet.addCardsTOView(cards: deck)
+    }
+    @IBAction func startNewGame(_ sender: UIButton) {
+        deck = DeckOfCards().deck
         deck.shuffle()
         playingTableOutlet.addCardsTOView(cards: deck)
     }
